c6e81cad65d340f44dcf3eb0e339b5cb3f6b1f44
Tosinpeter <tosyno826@gmail.com>
2023-06-28 10:46:50 +0100

    Update fluttermojiCircleAvatar.dart

--- lib/fluttermojiCircleAvatar.dart
+++ lib/fluttermojiCircleAvatar.dart
@@ -35,7 +35,7 @@ class FluttermojiCircleAvatar extends StatelessWidget {
           }
           return SvgPicture.string(
             snapshot.fluttermoji.value,
-            height: radius * 1.2,
+            height: radius * 1.9,
             semanticsLabel: "Your Fluttermoji",
             alignment: Alignment.bottomCenter,
             placeholderBuilder: (context) => Center(
